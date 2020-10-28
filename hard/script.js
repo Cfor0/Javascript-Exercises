25e80c29fc8ba20f240d0f0deb0c60ba6e8242c9
cfor0 <cfor0@outlook.com>
2020-10-27 20:49:51 -0400

    finished hard exercise

---
+++ hard/script.js
@@ -0,0 +1,14 @@
+const add = function(a, b) {
+    console.log(a + b)
+}
+const subtract = function(a, b) {
+    console.log(a - b)
+}
+const multiply = function(a, b) {
+    console.log(a * b)
+}
+const divide = function(a, b) {
+    console.log(a / b)
+}
+
+subtract(4, 6)
